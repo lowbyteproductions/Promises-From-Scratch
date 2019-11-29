165d6e77f16c2a38ea8033aa6019cdfff27b6396
Frank Stokes <frank@wearereasonablepeople.com>
2019-11-29 20:24:45 +0100

    First yield

---
+++ generators.js
@@ -0,0 +1,19 @@
+const interpreterLog = (...args) => console.log('[interpreter]', ...args);
+const producerLog = (...args) => console.log('\t[producer]', ...args);
+
+const producerGeneratorFn = function* (argumentValue) {
+  producerLog('function argument:', argumentValue);
+  yield 1;
+  yield 2;
+  return 3;
+};
+
+const interpreter = generatorFn => {
+  interpreterLog('Creating the generator object');
+  const producer = generatorFn("starting value");
+
+  let produced = producer.next();
+  interpreterLog('Got produced', produced);
+};
+
+interpreter(producerGeneratorFn);
